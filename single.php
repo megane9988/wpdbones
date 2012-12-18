47a78a65edf49a3cfc62f7c9edb4dce10af9b1ba
megane9988 <hajihaji9988@gmail.com>
2012-12-19 02:00:40 +0900

    次前の記事リンク及び投稿者過去記事を追加

--- single.php
+++ single.php
@@ -30,8 +30,32 @@
 								<div class="adtxt">
 									<?php do_action('wpdbones-ad-content-below'); ?>
 								</div>	
+<ul class="pn">
+	<li><?php previous_post_link('前の記事：%link を読む'); ?></li>
+	<li><?php next_post_link('次の記事：%link を読む'); ?></li>
+</ul>
 								<footer class="article-footer">
-			
+<div class="otherPosts">
+	<h3 class="<?php the_author_meta('display_name') ?>"><?php the_author_posts_link(); ?>の過去記事一覧</h3>
+	<div class="posts">
+		<?php
+            $author = get_the_author_meta('ID');
+			$otherPosts = get_posts(array(
+					'numberposts' => 5,
+                    'author' => $author
+				));
+			if($otherPosts) :
+		?>
+		<ul>
+		<?php foreach( $otherPosts as $op ) : ?>
+			<li><a href="<?php echo get_permalink($op->ID); ?>"><?php echo get_the_title($op->ID); ?></a></li>
+		<?php endforeach; ?>
+		</ul>
+		<?php else : ?>
+		<p>過去記事はまだありません。</p>
+		<?php endif; ?>
+	</div>
+</div>
 									<?php the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
 							
 								</footer> <!-- end article footer -->
@@ -50,7 +74,7 @@
 					    		</header>
 					    		<section class="entry-content">
 					    			<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-					    		</section>
+					    		</section>	
 					    		<footer class="article-footer">
 					    		    <p><?php _e("This is the error message in the single.php template.", "bonestheme"); ?></p>
 					    		</footer>
