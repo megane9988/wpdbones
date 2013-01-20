5cb79b80ba6283e2412da7ff617af0f9faa7925f
megane9988 <hajihaji9988@gmail.com>
2013-01-20 20:58:15 +0900

    スクロールについてくるバナーをスマホでは表示しないように

--- sidebar.php
+++ sidebar.php
@@ -3,12 +3,14 @@
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
 						<?php dynamic_sidebar( 'sidebar1' ); ?>
-
+						<?php if ( function_exists('wp_is_mobile') && wp_is_mobile() ) :?>
+						<?php else: ?>						
 						<div id="scrollside">
 						<h4 class="widgettitle">おすすめ図書</h4>
 						<a href="http://wp-d.org/2013/01/14/1938/"><img src="http://wp-d.org/wp-content/uploads/2013/01/book2012.jpg" alt="おすすめ図書"></a>
 						<p><a href="http://wp-d.org/2013/01/14/1938/">新春座談会 このコンピュータ書がすごい！ 2013年版</a></p>
 						</div>
+						<?php endif; ?>
 					<?php else : ?>
 
 						<!-- This content shows up if there are no widgets defined in the backend. -->
