a3519d6301e7f22cc3a8e0aa5f0f5ebe3f0a42d7
megane9988 <hajihaji9988@gmail.com>
2013-01-22 11:01:02 +0900

    サイドに楽天

--- sidebar.php
+++ sidebar.php
@@ -4,11 +4,15 @@
 
 						<?php dynamic_sidebar( 'sidebar1' ); ?>
 						<?php if ( function_exists('wp_is_mobile') && wp_is_mobile() ) :?>
+						<!-- Rakuten Widget FROM HERE --><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="0b6b446d.5a240e9c.0b6b448a.baba55ac";rakuten_items="ctsmatch";rakuten_genreId=0;rakuten_size="320x48";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="off";rakuten_auto_mode="off";rakuten_genre_title="off";rakuten_recommend="on";</script><script type="text/javascript" src="http://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script><!-- Rakuten Widget TO HERE -->
 						<?php else: ?>						
 						<div id="scrollside">
 						<h4 class="widgettitle">おすすめ図書</h4>
-						<a href="http://wp-d.org/2013/01/14/1938/"><img src="http://wp-d.org/wp-content/uploads/2013/01/book2012.jpg" alt="おすすめ図書"></a>
+						<a style="float: left; margin-right: 10px;"  href="http://wp-d.org/2013/01/14/1938/"><img src="http://wp-d.org/wp-content/uploads/2013/01/book2012.jpg" alt="おすすめ図書"></a>
 						<p><a href="http://wp-d.org/2013/01/14/1938/">新春座談会 このコンピュータ書がすごい！ 2013年版</a></p>
+						<div class="clearfix">
+					<!-- Rakuten Widget FROM HERE --><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="0b6b446d.5a240e9c.0b6b448a.baba55ac";rakuten_items="ctsmatch";rakuten_genreId=0;rakuten_size="336x280";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="off";rakuten_genre_title="off";rakuten_recommend="on";</script><script type="text/javascript" src="http://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script><!-- Rakuten Widget TO HERE -->
+						</div>
 						</div>
 						<?php endif; ?>
 					<?php else : ?>
