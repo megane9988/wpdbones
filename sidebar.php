93d4a5a099255e04d6f2da301c005df85dd242e4
megane9988 <hajihaji9988@gmail.com>
2013-01-29 03:15:20 +0900

    サイドバーのついてくる奴の修正

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,7 @@
 						<?php if ( function_exists('wp_is_mobile') && wp_is_mobile() ) :?>
 						<!-- Rakuten Widget FROM HERE --><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="0b6b446d.5a240e9c.0b6b448a.baba55ac";rakuten_items="ctsmatch";rakuten_genreId=0;rakuten_size="320x48";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="off";rakuten_auto_mode="off";rakuten_genre_title="off";rakuten_recommend="on";</script><script type="text/javascript" src="http://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script><!-- Rakuten Widget TO HERE -->
 						<?php else: ?>						
-						<div id="scrollside">
+						<div class="scrollside">
 						<h4 class="widgettitle">おすすめ図書</h4>
 						<a style="float: left; margin-right: 10px; "  href="http://wp-d.org/2013/01/14/1938/"><img style="width:100px;height:100px;" src="http://wp-d.org/wp-content/uploads/2013/01/book2012.jpg" alt="おすすめ図書"></a>
 						<p><a href="http://wp-d.org/2013/01/14/1938/">新春座談会 このコンピュータ書がすごい！ 2013年版</a></p>
