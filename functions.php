ec805e6b0a9522dea90a62419dba0a952a3ecfa2
megane9988 <hajihaji9988@gmail.com>
2013-04-11 23:27:16 +0900

    メインとサイド

--- functions.php
+++ functions.php
@@ -139,10 +139,6 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 
 function wpdbones_ad_content_side(){
 if ( function_exists('wp_is_mobile') && wp_is_mobile() ) :?>
-
-<h4 class="widgettitle">WP-D監修の本が出ます！</h4>
-            <a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_tl?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank">プロが選ぶ WordPress優良プラグイン事典</a><img src="http://www.assoc-amazon.jp/e/ir?t=hajihaji9988-22&l=as2&o=9&a=4844363298" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-            <a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_il?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank"><img border="0" src="http://wp-d.org/wp-content/uploads/2013/03/pro.jpg" ></a>  
 <div id="socialside" class="socialwidget">
 <p style="background-color: #145470;color: #fff;padding: 10px;margin-top: 0px;">フォローして新着情報をキャッチ！</p>
 <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FWP-D%2F292801787485325&amp;width=292&amp;height=62&amp;show_faces=false&amp;colorscheme=light&amp;stream=false&amp;border_color&amp;header=false&amp;appId=269852913138083" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:62px;" allowTransparency="true"></iframe>
@@ -163,10 +159,6 @@ google_ad_height = 60;
 src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>
 <?php else: ?>
-
-  <h4 class="widgettitle">WP-D監修の本が出ます！</h4>
-            <a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_tl?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank">プロが選ぶ WordPress優良プラグイン事典</a><img src="http://www.assoc-amazon.jp/e/ir?t=hajihaji9988-22&l=as2&o=9&a=4844363298" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-            <a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_il?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank"><img border="0" src="http://wp-d.org/wp-content/uploads/2013/03/pro.jpg" ></a>
 <div id="socialside" class="socialwidget">
 <p style="background-color: #145470;color: #fff;padding: 10px;margin-top: 0px;">フォローして新着情報をキャッチ！</p>
 <div class="fb-like-box" style="background-color:#fff;width: 100%;">
--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,10 @@
 						<?php if ( function_exists('wp_is_mobile') && wp_is_mobile() ) :?>
 						<?php else: ?>						
 						<div class="scrollside">
-
-						<h4 class="widgettitle">WP-D監修の本が出ます！</h4>
-						<a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_tl?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank">プロが選ぶ WordPress優良プラグイン事典</a><img src="http://www.assoc-amazon.jp/e/ir?t=hajihaji9988-22&l=as2&o=9&a=4844363298" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
-						<a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_il?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank"><img border="0" src="http://wp-d.org/wp-content/uploads/2013/03/pro.jpg" ></a>
-						<h4 class="widgettitle">おすすめ図書</h4>
+						<h4 class="widgettitle">おすすめ図書</h4>						
+						<a style="float: left; margin-right: 10px; "  href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_il?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank"><img style="width:100px;" src="http://ws.assoc-amazon.jp/widgets/q?_encoding=UTF8&ASIN=4844363298&Format=_SL160_&ID=AsinImage&MarketPlace=JP&ServiceVersion=20070822&WS=1&tag=hajihaji9988-22" alt="おすすめ図書"></a>
+						<p><a href="http://www.amazon.co.jp/gp/product/4844363298/ref=as_li_ss_il?ie=UTF8&camp=247&creative=7399&creativeASIN=4844363298&linkCode=as2&tag=hajihaji9988-22" target="_blank">WP-D監修の本が出ました！プロが選ぶ WordPress優良プラグイン事典</a></p>
+						<div style="clear: both"></div>
 						<a style="float: left; margin-right: 10px; "  href="http://wp-d.org/2013/01/24/2146/"><img style="width:100px;height:100px;" src="http://wp-d.org/wp-content/uploads/2013/01/kindle-200x200.jpg" alt="おすすめ図書"></a>
 						<p><a href="http://wp-d.org/2013/01/24/2146/">PHPをよく知らずにWordPressを扱う人にぜひ読んでほしい電子書籍の紹介（著者インタビュー）</a></p>
 						<div style="clear: both"></div>
