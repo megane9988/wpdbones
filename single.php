459a677efe4cdb5d5391d8d28edb1e607cab9ee4
megane9988 <hajihaji9988@gmail.com>
2013-04-25 23:48:49 +0900

    日付をタイトル付近にも導入

--- single.php
+++ single.php
@@ -17,6 +17,8 @@
 									<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 						
 								</header> <!-- end article header -->
+									<p class="byline vcard"><time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" ><?php the_time('Y年m月d日（D）'); ?></time> | <span class="author <?php the_author(); ?>"><?php the_author_posts_link(); ?></span> <span class="amp"></span> | <?php the_category(', '); ?> | <?php comments_number(); ?>
+									</p>
 								<section class="entry-content clearfix" itemprop="articleBody">
 									<?php the_content(); ?>
 								</section> <!-- end article section -->
